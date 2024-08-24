9f73c2627b1ce128fc263a45ef549018494db8b4
thrilokprakashh <thrilokprakashan@gmail.com>
2024-08-24 16:47:02 +0530

    forgotPassword done

---
+++ lib/view/forgotpassword/forgot_password.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:validation_wrk/view/checkemail/check_email.dart';
+
+class ForgotPassword extends StatefulWidget {
+  const ForgotPassword({super.key});
+
+  @override
+  State<ForgotPassword> createState() => _ForgotPasswordState();
+}
+
+final _formKey = GlobalKey<FormState>();
+final _emailController = TextEditingController();
+String checkEmail = "";
+
+class _ForgotPasswordState extends State<ForgotPassword> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Forgot Password',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                "Please enter your email to reset the password",
+                style: TextStyle(
+                    color: Colors.black54,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 13),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              TextFormField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                  labelText: "Enter your email",
+                  labelStyle: TextStyle(color: Colors.black54),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(color: Colors.black38),
+                  ),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your email';
+                  }
+                  {
+                    if (!value.contains('@')) {
+                      return 'Please enter a valid email address';
+                    }
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: WidgetStatePropertyAll(Colors.blue),
+                    foregroundColor: WidgetStatePropertyAll(Colors.white),
+                    padding: WidgetStatePropertyAll(
+                      EdgeInsets.symmetric(horizontal: 140, vertical: 15),
+                    ),
+                    shape: WidgetStatePropertyAll(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                  ),
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      checkEmail = _emailController.text;
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => CheckEmail(),
+                        ),
+                      );
+                    }
+                  },
+                  child: Text("Reset Password"))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
